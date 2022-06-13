d2b3da6312d85bb396101ee6abac2913c2caeed8
isablaquier <iblaquier@udesa.edu.ar>
2022-06-13 17:35:55 -0300

    hice generos

---
+++ js/detallegeneros.js
@@ -0,0 +1,52 @@
+let qs = location.search;
+let qsOL = new URLSearchParams(qs);
+let id = qsOL.get('idGenero');
+
+let endpoint = `https://api.allorigins.win/raw?url=https://api.deezer.com/genre/${id}`
+
+fetch(endpoint)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) { 
+    console.log(data);
+    
+    let etiquetaGenero = document.querySelector('#tituloGenero');
+    etiquetaGenero.innerText = `Genero : ${data.name}`
+   
+
+}).catch(function(error) {
+    console.log(error);
+})
+
+
+let url = `https://api.allorigins.win/raw?url=https://api.deezer.com/genre/${id}/artists`
+
+fetch(url)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) { 
+   
+    let contenido = '';
+
+    let artistas = data.data;
+
+    console.log(artistas);
+    
+    let etiquetaArtistas = document.querySelector('#artistasGenero');
+    
+    for (let i = 0; i < 3 ;i++) {
+        contenido += `<div class="hola"> 
+                                    <a href="detallegeneros.html">  <img src="${artistas[i].picture}" alt="" class="imgcanciones" ></a>
+                                    <h2 class="nombress">${artistas[i].name}</h2>
+                                </div> `
+        
+    }
+
+    etiquetaArtistas.innerHTML = contenido;
+   
+
+}).catch(function(error) {
+    console.log(error);
+})
---
+++ js/generos.js
@@ -0,0 +1,26 @@
+
+let endpoint = 'https://api.allorigins.win/raw?url=https://api.deezer.com/genre/'
+    
+        
+fetch(endpoint)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) { 
+        console.log(data);
+        for (let i = 1; i < 7; i++) {
+            document.querySelector(`.article1`).innerHTML += `
+            <div> 
+            <a href="./detallegeneros.html?idGenero=${data.data[i].id}">  <img src="${data.data[i].picture}" alt="" class="imgcanciones" ></a>
+            <h2 class="nombress">${data.data[i].name}</h2>
+
+        </div> 
+        <hr>` 
+            
+        }
+      
+
+    }).catch(function(error) {
+        console.log(error);
+    })
+
