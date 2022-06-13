50da157474ac82aed845673c2d7323c8a2225553
MiguelGuerrer <guerrerom@udesa.edu.ar>
2022-06-13 17:41:20 -0300

    pagina home

---
+++ index.js
@@ -0,0 +1,41 @@
+fetch("https://api.allorigins.win/raw?url=https://api.deezer.com/chart")
+.then(function(response){
+    return response.json();
+})
+.then(function (data) {
+
+    console.log(data);
+
+        let topCanciones = data.tracks.data
+        let topAlbums = data.albums.data
+        let topArtistas = data.artists.data
+        
+
+        let cancion = document.querySelector(".topcanciones")
+        let album = document.querySelector(".topalmbumes")
+        let artista = document.querySelector(".topartistas")
+
+
+    for (let i = 0; i < 5; i++) {
+        cancion.innerHTML += `<div class="div1"> 
+        <a href="./detalleartistas.html?id=${topCanciones[i].id}"><img  class="foto" src=${topCanciones[i].album.cover_big}></a>
+        <a class="ahome" href="./detalleartistas.html"><h4  class="canciones2322" >${topCanciones[i].title_short}</h4></a> 
+        </div>`
+    } 
+       
+    for (let i = 0; i < 5; i++) {
+        album.innerHTML += `<div class="div1"> 
+        <a href="./detalleartistas.html?id=${topAlbums[i].id}"><img  class="foto" src=${topAlbums[i].cover_big}></a>
+        <a class="ahome" href="./detalleartistas.html"><h4  class="albums2322" >${topAlbums[i].title}</h4></a> 
+        </div>`
+    }
+ 
+    for (let i = 0; i < 5; i++) {
+        artista.innerHTML += `<div class="div1"> 
+        <a href="./detalleartistas.html?id=${topArtistas[i].id}"><img  class="foto" src=${topArtistas[i].picture_big}></a>
+        <a class="ahome" href="./detalleartistas.html"><h4  class="albums2322" >${topArtistas[i].name}</h4></a> 
+        </div>`
+    }
+})
+
+    
