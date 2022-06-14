d956b254fa198fa09347f7a9c7c422c2b7cd6d0d
isablaquier <iblaquier@udesa.edu.ar>
2022-06-14 11:07:38 -0300

    termine el buscador

--- js/resultados.js
+++ js/resultados.js
@@ -2,25 +2,16 @@ let objeto = new URLSearchParams (location.search);
 let search = objeto.get("search");
 
 fetch(`https://api.allorigins.win/raw?url=https://api.deezer.com/search?q=${search}`)
-.then( respuesta =>{
+.then( function(respuesta){
     return respuesta.json()
 })
-.then( busqueda =>{
-    console.log(busqueda)
+.then( function(busqueda){
+    console.log()
     
     let resultados = busqueda.data
     let resultadosB = document.querySelector(".articulo1")
-let encabezado = document.querySelector(".busqueda")
-    encabezado.innerHTML +=
-
-   ` <h2 class="h3"> Usted está buscando :${search}</h2> `
-
-    for( i= 0; i <= resultados.length; i++){
-
-    resultadosB.innerHTML +=
-    
-    `
-        <h4><a href="detallecancion.html?id=${resultados[i].id}">${resultados[i].title}</a></h4>
-    `
-        }
+    let encabezado = document.querySelector("#busqueda")
+    for(let i = 0; i < resultados.length; i++){
+        resultadosB.innerHTML += `<h4><a class="busqueda-palabra" href="detallecancion.html?id=${busqueda.data[i].id}">${busqueda.data[i].title}</a></h4>`
+    }
 })
