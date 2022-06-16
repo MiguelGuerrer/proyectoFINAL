12175dd93f6ee1a3d9c7da9240b94c9b166f1918
isablaquier <iblaquier@udesa.edu.ar>
2022-06-16 13:21:12 -0300

    modifique cosas en el buscador

--- js/resultados.js
+++ js/resultados.js
@@ -6,13 +6,25 @@ fetch(`https://api.allorigins.win/raw?url=https://api.deezer.com/search?q=${sear
     return respuesta.json()
 })
 .then( function(busqueda){
-    console.log()
+    console.log(busqueda.data)
     
     let resultados = busqueda.data
     let resultadosB = document.querySelector(".articulo1")
     let encabezado = document.querySelector("#busqueda")
     for(let i = 0; i < resultados.length; i++){
-        resultadosB.innerHTML += `<h4><a class="busqueda-palabra" href="detallecancion.html?id=${busqueda.data[i].id}">${busqueda.data[i].title}</a></h4>`
+
+
+            resultadosB.innerHTML += `
+            
+    
+
+
+            <div class="div1"> 
+        <a href="./detalleartistas.html?id=${busqueda.data[i].id}"><img  class="foto" src=${busqueda.data[i].album.cover}></a>
+        <a class="ahome" href="./detalleartistas.html"><h4  class="albums2322" >${busqueda.data[i].title}</h4></a> 
+        </div>
+            `
+
     }
 })
 
