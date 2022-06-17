d528f61f53534330d8b8775f814f6552800bc25d
MiguelGuerrer <guerrerom@udesa.edu.ar>
2022-06-17 12:00:52 -0300

    pagina detalletrack, boton y playlist

--- js/detallecancion.js
+++ js/detallecancion.js
@@ -1,25 +1,15 @@
-let queryString = location.search //Caputro del url toda la qs
-let queryObject = new URLSearchParams(queryString); //La transformamos en OL por new url..
-let id = queryObject.get('id');//obtengo el id y lo pongo en url
+let
 
-let url = `https://api.allorigins.win/raw?url=https://api.deezer.com/track/3135556${queryObject}`;
 
-fetch( url )
-.then( function(response){
-return response.json();//convertimos la info en formato json
-})
-.then(function(data){
-    console.log(data);
-    //devuelve info de 1 track en innerText/HTML
-    //usamos innerText e innerHTML para cambiar lo que esta dentro de las etiquetas del html
 
+/*
     let cancion = document.querySelector('.centrado'); 
     //creamos ruta hacia nombre track
     cancion.innerText += data.title;
 
     let img = document.querySelector(".imgDetailCancion");
     //creamos ruta hacia foto
-    img.src += data.album.cover_big;
+    img.src += data.album.cover_medium;
 
     let artista = document.querySelector(".centrado2");
     //creamos ruta hacia nombre artista
@@ -33,4 +23,4 @@ return response.json();//convertimos la info en formato json
     //creamos ruta hacia iframe
     player.innerHTML +=`<iframe src="https://open.spotify.com/embed/track/41wtwzCZkXwpnakmwJ239F${data.link}" width="100%" height="300" frameBorder="0"></iframe>`;
 
-})
+})*/
